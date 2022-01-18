55597ca45624cbfb8c796fa053ebe426a2d1c6bc
mxllx.c <mille.ouriques@gmail.com>
2022-01-18 09:08:48 -0300

    done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+import { ReactChild, ReactFragment, ReactPortal } from "react";
+import { MovieCard } from "./MovieCard";
+
+export function Content(props: { selectedGenre: { title: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }; movies: any[]; }) {
+  return(
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { Button } from "./Button";
+
+export function SideBar(props: { setSelectedGenreId: (arg0: number) => void; genres: any[]; selectedGenreId: any; }) {
+
+
+
+  
+
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
